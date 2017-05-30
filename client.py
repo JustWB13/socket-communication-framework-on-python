c21569bb41b61f7437a27d4803ffc85c2de4c6f9
justwb <1242705299@qq.com>
2017-05-30 21:49:09 +0800

    some update

--- client.py
+++ client.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import os
 import socket
 import time
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-s.connect(('192.168.2.108',1027))
+s.connect(('10.3.45.144',1027))
 
 print(s.recv(1024).decode('utf-8'))
 while True:
