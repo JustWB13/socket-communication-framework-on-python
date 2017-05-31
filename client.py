cd255b00f5cbc0cb23846f272c1afdd3240ba511
justwb <1242705299@qq.com>
2017-05-31 11:23:54 +0800

    some update

--- client.py
+++ client.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from multiprocessing import Process
 import os
 import socket
 import time
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
 s.connect(('10.3.45.144',1027))
+temp=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+
+
+def udpget():
+    temp.bind((myaddr,1027))
+    while True:
+        if(a==1):break
+        data,addr=temp.recvfrom(1024)
+        print(data.decode('utf-8'))
+
+
 
 print(s.recv(1024).decode('utf-8'))
+myaddr=s.recv(1024).decode('utf-8')
+rec=Process(target=udpget,args=())
+rec.start()
 while True:
     data=input()
     s.send(data.encode())
--- server.py
+++ server.py
@@ -7,25 +7,38 @@ import socket
 import time
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind(('10.3.45.144',1027))
+s.listen(10)
 
-s.bind(('192.168.2.108',1027))
 
-s.listen(5)
-print("waiting...")
+usend=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
+
+ADDR=['127.0.0.1']
 
 def Tcp(sock,addr):
     print("connection from %s:%s..."%addr)
-    sock.send(b'Welcome')
-    data=sock.recv(1024)
-    while data.decode('utf-8')!='exit':
-        print(data.decode('utf-8'))
+    sock.send(b'Please enter your name:')
+    sock.send(addr[0].encode())
+    usrname=sock.recv(1024)
+    while True:
         data=sock.recv(1024)
+        if data.decode('utf-8')=='exit':break
+        sen=Process(target=udpsend,args=(data.decode('utf-8'),addr[0]))
+        sen.start()
     sock.close()
     print("connection end(%s:%s)"%addr)
 
+def udpsend(data,addr):
+    for con in ADDR:
+        if con=='127.0.0.1':continue
+        if con==addr:continue
+        usend.sendto(data.encode(),(con,1027))
 
+
+print("Waiting...")
 while True:
     sock,addr=s.accept()
+    ADDR.append(addr[0])
     p=Process(target=Tcp,args=(sock,addr))
     p.start()
